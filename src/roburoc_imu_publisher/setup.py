473728e4b75fb011079253912ddc204bd47cbbe1
Norbert Pap <garrote713@gmail.com>
2026-04-19 18:36:00 +0200

    fix: correct entry point for imu_publisher_node in setup.py

--- src/roburoc_imu_publisher/setup.py
+++ src/roburoc_imu_publisher/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'roburoc_imu_publisher = roburoc_imu_publisher.roburoc_imu_publisher_node:main'
+            'roburoc_imu_publisher = roburoc_imu_publisher.imu_publisher_node:main'
         ],
     },
 )
